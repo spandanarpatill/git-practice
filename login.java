995a12e227b10ea4815bd6bea64059c6739bd0d3
spandanarpatil <spandanarpatil2002@gmail.com>
2026-08-11 21:29:09 +0530

    Updated login in main

--- login.java
+++ login.java
@@ -1,1 +1,6 @@
-login.java
+login.javapublic class Login {
+    public static void main(String[] args) {
+        System.out.println("Login Page");
+        System.out.println("Login page updated from main");
+    }
+}
